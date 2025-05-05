8cc90f8cea57609b20d12a96aed19180454087bd
MaiaAndrey31 <maiaandrey31@outlook.com>
2025-05-05 16:29:07 -0300

    Feat Home

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,18 @@
+import { Banner, Container, Content } from "./styles";
+
+export function Home() {
+    return (
+        <main>
+            <Banner>
+            <h1>Bem-Vindo(a)!</h1>
+        </Banner>
+        <Container>
+           <Content>
+           <div>Carrossel Categorias </div>
+           <div>Carrossel Produtos</div>
+           </Content>
+
+        </Container>
+        </main>
+    )
+}
---
+++ src/containers/Home/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+import BannerHome from '../../assets/Banner.svg'
+import Background from '../../assets/bg-2.svg'
+import { theme } from "../../styles/theme"
+
+export const Banner = styled.div`
+background: url('${BannerHome}');
+background-size: cover;
+background-position: center;
+height: 400px;
+
+h1{
+    font-family: 'Road Rage', sans-serif;
+    font-weight: 500;
+    font-size: 80px;
+    color: ${theme.colors.lighttext};
+    position: absolute;
+    right: 10%;
+    top: 5%;
+}
+`
+
+export const Container = styled.section`
+background: linear-gradient(rgba(255,255,255, 0.4),rgba(255,255,255, 0.4)),
+
+url('${Background}');
+height: 500px;
+
+
+`
+
+export const Content = styled.div``
