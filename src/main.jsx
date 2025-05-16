a3c8fdd58e52dab545a276a1897aa50b2aaed351
MaiaAndrey31 <maiaandrey31@outlook.com>
2025-05-16 16:48:28 -0300

    Feat Outlet Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import GlobalStyles from './styles/globalStyles'
 import { ToastContainer } from 'react-toastify'
-import { RouterProvider } from 'react-router-dom'
-import { routes } from './routes'
+import { BrowserRouter } from 'react-router-dom'
 import AppProvider from './hooks'
+import { Router } from './routes'
 import { Elements } from '@stripe/react-stripe-js'
 import stripePromise from './config/stripeConfig'
 
@@ -12,7 +12,9 @@ createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
       <Elements stripe={stripePromise}>
-        <RouterProvider router={routes} />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </Elements>
       <GlobalStyles />
       <ToastContainer autoClose={2500} theme="dark" />
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,47 +1,34 @@
-import { createBrowserRouter } from 'react-router-dom'
 
-import { Header, Footer } from '../components'
-import { Cart, Home, Menu, Register, Login, Checkout, CompleteOrder } from '../containers'
+import {
+  Cart,
+  Home,
+  Menu,
+  Register,
+  Login,
+  Checkout,
+  CompleteOrder
+} from '../containers'
+import { UserLayout } from '../layouts/UserLayout'
 
-export const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <>
-        <Header />
-        <Home />
-        <Footer />
-      </>
-    )
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/cadastro',
-    element: <Register />
-  },
-  {
-    path: '/cardapio',
-    element: (
-      <>
-        <Header />
-        <Menu />
-        <Footer />
-      </>
-    )
-  },
-  {
-    path: '/carrinho',
-    element: <Cart />
-  },
-  {
-    path: '/checkout',
-    element: <Checkout />
-  },
-  {
-    path: '/complete',
-    element: <CompleteOrder />
-  }
-])
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<UserLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cardapio" element={<Menu />} />
+        <Route path="/carrinho" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/complete" element={<CompleteOrder />} />
+      </Route>
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Register />} />
+    </Routes>
+  )
+}
+// 
+//   {
+//     path: '/complete',
+//     element: <CompleteOrder />
+//   }
+// ])
